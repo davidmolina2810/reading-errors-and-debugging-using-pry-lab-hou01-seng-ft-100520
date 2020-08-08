b2d84e566117eb9ebdfcc0037d42adaadccdf263
David Molina <davidmolina2810@gmail.com>
2020-08-08 08:00:50 +0000

    Automatically backed up by Learn

--- fix_using_tests/pemdos.rb
+++ fix_using_tests/pemdos.rb
@@ -1,9 +1,8 @@
 # don't forget to add: require 'pry'
 require 'pry'
 def snake_it_up(string)
-  binding.pry
   if string[0] == "s"
-    ("s"*10) + string
+    string = ("s"*10) + string
   else
     string
   end
